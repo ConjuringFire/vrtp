9d9bdb895c6f1281fd836203a34bf0e90c738839
ConjuringFire <bnthompson87@icloud.com>
2025-03-07 17:50:16 +1000

    updated the details page's styling

--- src/app/brewery/[id]/page.tsx
+++ src/app/brewery/[id]/page.tsx
@@ -1,4 +1,4 @@
-import BreweryDetails from '@/components/BreweryDetails';
+import BreweryDetails from '@/components/brewery/BreweryDetails/BreweryDetails';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,5 +6,11 @@ export const metadata: Metadata = {
 };
 
 export default function BreweryDetailPage() {
-    return <BreweryDetails />;
+    return (
+        <div className="bg-linear-to-bl from-[#FF3A63] to-[#D72389]">
+            <div className="max-w-screen-lg mx-auto w-full drop-shadow-xl">
+                <BreweryDetails />
+            </div>
+        </div>
+    );
 }
---
+++ src/components/brewery/BreweryDetails/BreweryDetails.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useBreweryDetails } from '@/hooks/useBreweryDetails';
+import { useParams } from 'next/navigation';
+import { Suspense } from 'react';
+import BreweryMap from '../MapComponent';
+
+/**
+ * a brewery details component for displaying deails about a specific brewery
+ *
+ * @returns {JSX.Element} the rendered BreweryDetails component
+ */
+const BreweryDetails = () => {
+    // get brewery ID from route parameters
+    const { id } = useParams();
+
+    return (
+        <Suspense fallback={<p>Loading brewery details...</p>}>
+            <BreweryDetailsContent id={id as string} />
+        </Suspense>
+    );
+};
+
+const BreweryDetailsContent = ({ id }: { id: string }) => {
+    const { brewery, error } = useBreweryDetails({ id: id as string });
+
+    // display error message if fetching brewery details failed
+    if (error) {
+        return <p>Error: {error}</p>;
+    }
+
+    // display error message if brewery is not found
+    if (!brewery) {
+        return <p>Brewery not found</p>;
+    }
+
+    return (
+        <div className="p-4 text-black">
+            <h1 className="text-2xl font-semibold mb-12">{brewery.name}</h1>
+            <div className="bg-white rounded-2xl shadow-md p-4 mb-12">
+                <h2 className="text-lg font-semibold mb-2">Details</h2>
+                <p>
+                    <strong>Type:</strong> {brewery.brewery_type}
+                </p>
+                <p>
+                    <strong>Address:</strong> {brewery.street}, {brewery.city},{' '}
+                    {brewery.state}, {brewery.postal_code}, {brewery.country}
+                </p>
+                <p>
+                    <strong>Phone:</strong> {brewery.phone}
+                </p>
+                <p>
+                    <strong>Website: </strong>
+                    <a
+                        href={brewery.website_url || ''}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {brewery.website_url}
+                    </a>
+                </p>
+            </div>
+            <div className="bg-white rounded-2xl shadow-md p-4">
+                <h2 className="text-lg font-semibold mb-2">Map</h2>
+                <Suspense fallback={<p>Loading Map...</p>}>
+                    <BreweryMap
+                        latitude={brewery.latitude}
+                        longitude={brewery.longitude}
+                    />
+                </Suspense>
+            </div>
+        </div>
+    );
+};
+
+export default BreweryDetails;
