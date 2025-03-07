6d77460c5d741f0809e90e7f1eb860b0c51a32ad
ConjuringFire <bnthompson87@icloud.com>
2025-03-07 10:11:55 +1000

    updated app title and metadata

--- src/app/brewery/[id]/page.tsx
+++ src/app/brewery/[id]/page.tsx
@@ -1,4 +1,9 @@
 import BreweryDetails from '@/components/BreweryDetails';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Brewery'
+};
 
 export default function BreweryDetailPage() {
     return <BreweryDetails />;
